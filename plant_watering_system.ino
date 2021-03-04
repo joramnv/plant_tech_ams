642cc125bd83f9bcc9cf0837ec47af8e94a77e34
Joram Visser <joram.n.v@gmail.com>
2021-03-04 20:17:31 +0100

    Using analog readings and millis instead of delay.

--- plant_watering_system.ino
+++ plant_watering_system.ino
@@ -4,7 +4,8 @@
 double analogValue = 0.0;
 int digitalValue = 0;
 double analogVolts = 0.0;
-unsigned long timeHolder = 0;
+unsigned long previousMillis = 0;
+unsigned long intervalMillis = 0;
 void setup() {
   // put your setup code here, to run once:
   pinMode(ANA, INPUT);
@@ -14,19 +15,26 @@ void setup() {
   Serial.println("Paolo Test");
 }
 void loop() {
-  analogValue = analogRead(ANA);
-  digitalValue = digitalRead(DIGI);
-  // Serial data
-  Serial.print("Analog raw: ");
-  Serial.println(analogValue);
-  Serial.print("Digital raw: ");
-  Serial.println(digitalValue);
-  Serial.println(" ");
-  if (digitalValue == 0) {
-    digitalWrite(POMPOUT, false);
-  } else {
-        Serial.println("PUMPIN");
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= intervalMillis) {
+    analogValue = analogRead(ANA);
+    digitalValue = digitalRead(DIGI);
+    // Serial data
+    Serial.print("Analog raw: ");
+    Serial.println(analogValue);
+    Serial.print("Digital raw: ");
+    Serial.println(digitalValue);
+    Serial.println(" ");
+    if (analogValue < 600) {
+      digitalWrite(POMPOUT, false);
+      //delay(5000);
+      intervalMillis = 5000;
+    } else {
+      Serial.println("PUMPIN");
       digitalWrite(POMPOUT, true);
+      //delay(1000);
+      intervalMillis = 1000;
+    }
+    previousMillis = currentMillis;
   }
-  delay(1000);
 }
